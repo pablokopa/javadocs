934458b03224d5d2ec6ce37b59017a495a0835f4
PabloIglesiasL <piglesiasleyenda@danielcastelao.org>
2023-11-20 09:13:56 +0100

    first commit

---
+++ Entrega/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		SumaResta obj=new SumaResta();
+		obj.restarNum(1,2);
+		obj.restarNum(2,3);
+	}
+}
---
+++ Entrega/src/SumaResta.java
@@ -0,0 +1,32 @@
+/**
+ * Suma y Resta
+ *
+ * @author Pablo Iglesias Leyenda
+ * @version v1.0
+ * */
+public class SumaResta {
+
+	/**
+	 * Suma
+	 *
+	 * @param num1 Número 1.
+	 * @param num2 Número 2.
+	 *
+	 * @return Suma de los números
+	 * */
+	public int sumarNum (int num1, int num2){
+		return num1+num2;
+	}
+
+	/**
+	 * Resta
+	 *
+	 * @param num1 Número 1.
+	 * @param num2 Número 2.
+	 *
+	 * @return Resta de los números
+	 * */
+	public int restarNum (int num1, int num2){
+		return num1-num2;
+	}
+}
